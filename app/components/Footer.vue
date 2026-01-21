78d357ffc4bf8bb691c1c970120a050c1e05384a
Goodness Urama <goodyboy301@gmail.com>
2026-01-21 08:24:47 +0100

    re: init

---
+++ app/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="footer">
+    <div class="footer-top">
+      <h6>
+        Justice should never depend on money, power, or influence. Whether you’re seeking help or supporting our work, your action
+        makes protection possible.
+      </h6>
+      <div class="footer-top-ctas">
+        <button data-secondary>Get Help</button>
+        <button data-primary>Make a Donation</button>
+      </div>
+    </div>
+    <figure class="footer-media">
+      <img src="/images/footer.webp" alt="" />
+    </figure>
+    <div class="footer-bottom">
+      <div class="footer-bottom-left">
+        <h5>Navigation</h5>
+        <div>
+          <nav>
+            <h6>Website</h6>
+            <NuxtLink href="/">Home</NuxtLink>
+            <NuxtLink href="/about">About</NuxtLink>
+            <NuxtLink href="/stories">Our Stories</NuxtLink>
+            <NuxtLink href="/donate">Donations</NuxtLink>
+          </nav>
+          <nav>
+            <h6>Socials</h6>
+            <NuxtLink href="https://x.com/rose.foundation_">Twitter</NuxtLink>
+            <NuxtLink href="https://instagram.com/rose.foundation_">Instagram</NuxtLink>
+            <NuxtLink href="https://linkedin.com/rose.foundation_">LinkedIn</NuxtLink>
+          </nav>
+        </div>
+        <p>© 2026 Rose Foundation. All rights reserved.</p>
+      </div>
+      <div class="footer-bottom-right">
+        <h5>Space Locations</h5>
+        <div>
+          <nav>
+            <h6>Nsukka, EN</h6>
+            <address>Plot E,<br />Students Union Building (SUB),<br />University of Nigeria, Nsukka.</address>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ app/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="header-logo">
+      <SvgoLogo filled />
+    </div>
+    <nav class="header-nav">
+      <NuxtLink href="/">Home</NuxtLink>
+      <NuxtLink href="/about">About</NuxtLink>
+      <NuxtLink href="/stories">Stories</NuxtLink>
+    </nav>
+    <button class="header-cta">Donate Today</button>
+  </header>
+</template>
