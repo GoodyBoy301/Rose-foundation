d40829a8abf449ed75ff8be6b78775e3d03f1904
Goodness Urama <goodyboy301@gmail.com>
2026-01-23 05:36:15 +0100

    Enhance header and footer styles for responsiveness and improve hamburger menu functionality

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -3,11 +3,33 @@
     <div class="header-logo">
       <SvgoLogo filled />
     </div>
-    <nav class="header-nav">
+    <nav class="header-nav" :class="{ isOpen }">
       <NuxtLink href="/">Home</NuxtLink>
       <NuxtLink href="/about">About</NuxtLink>
       <NuxtLink href="/stories">Stories</NuxtLink>
     </nav>
     <button class="header-cta">Donate Today</button>
+    <div class="header-hamburger first" data-mobile @click="isOpen = !isOpen">
+      <div class="line-one" />
+      <div class="line-two" />
+      <div class="line-three" />
+    </div>
   </header>
 </template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+const route = useRoute();
+onMounted(() => {
+  const burger = document.querySelector(".header-hamburger")!;
+  let first = true;
+  watch(isOpen, () => {
+    if (first) {
+      burger.classList.remove("first");
+      first = false;
+    }
+    burger.classList.toggle("animate");
+  });
+  watch(route, () => (isOpen.value = false));
+});
+</script>
